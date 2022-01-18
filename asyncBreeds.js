8d08d25345d347694331cf8ed80024d1d88377b2
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-18 14:51:17 -0500

    added tests and refactored syncBreed and asyncBreed

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,7 +6,7 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback (data);
+    callback (data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
@@ -16,6 +16,9 @@ const printCatBreed = (breed) =>{
   console.log(`Return Value: ${breed}`)
 }
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', printCatBreed);
-// console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+// // we try to get the return value
+// const bombay = breedDetailsFromFile('Bombay', printCatBreed);
+// // console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+
+
+module.exports = breedDetailsFromFile;

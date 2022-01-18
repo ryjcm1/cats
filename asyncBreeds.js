76211ba576f9bbec5846b2d1ce69ab77cf829212
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-18 14:52:11 -0500

    fixed formating

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,15 +6,15 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    callback (data);
+    callback(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
 const printCatBreed = (breed) =>{
-  console.log(`Return Value: ${breed}`)
-}
+  console.log(`Return Value: ${breed}`);
+};
 
 // // we try to get the return value
 // const bombay = breedDetailsFromFile('Bombay', printCatBreed);
